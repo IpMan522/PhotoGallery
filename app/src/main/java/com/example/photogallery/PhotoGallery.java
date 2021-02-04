bbf15b28b93975a77ca1bfe053212232867ff69c
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-04 15:34:46 +0700

    Добавил поиск

--- app/src/main/java/com/example/photogallery/PhotoGallery.java
+++ app/src/main/java/com/example/photogallery/PhotoGallery.java
@@ -1,6 +1,7 @@
 package com.example.photogallery;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,10 +14,14 @@ import com.example.photogallery.model.Response;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class PhotoGallery extends AppCompatActivity {
 
     RecyclerView rv;
+    Retrofit r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +31,7 @@ public class PhotoGallery extends AppCompatActivity {
         rv = findViewById(R.id.rv);
         rv.setLayoutManager(new GridLayoutManager(this, 3));
 
-        Retrofit r = ServiceAPI.getRetrofit();
+        r = ServiceAPI.getRetrofit();
         r.create(FlickrAPI.class).getRecent().enqueue(new Callback<Response>()
         {
             @Override
@@ -39,5 +44,46 @@ public class PhotoGallery extends AppCompatActivity {
             public void onFailure(Call<Response> call, Throwable t) {
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        final SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                r.create(FlickrAPI.class).getSearchPhotos(query).enqueue(
+                        new Callback<Response>()
+                        {
+                            @Override
+                            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                                PhotoAdapter pa = new PhotoAdapter(this, response.body().getPhotos().getPhoto());
+                                rv.setAdapter(pa);
+                            }
+
+                            @Override
+                            public void onFailure(Call<Response> call, Throwable t) {
+                            }
+                        }
+                );
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(queryTextListener);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
