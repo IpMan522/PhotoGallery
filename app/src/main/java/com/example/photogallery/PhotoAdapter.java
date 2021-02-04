d6ff1788bf9b9ff05e470b246ee2c5242b2cbed1
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-04 14:20:57 +0700

    Дописал код, чтобы все работало

--- app/src/main/java/com/example/photogallery/PhotoAdapter.java
+++ app/src/main/java/com/example/photogallery/PhotoAdapter.java
@@ -1,5 +1,6 @@
 package com.example.photogallery;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -7,27 +8,55 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.photogallery.db.PhotosDB;
+import com.example.photogallery.db.PhotosDao;
+import com.example.photogallery.model.Photo;
+import com.example.photogallery.model.Response;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import retrofit2.Callback;
+
 public class PhotoAdapter extends RecyclerView.Adapter <PhotoAdapter.ViewHolder> {
-    @NonNull
-    @Override
-    public PhotoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+    private final Callback<Response> photoGallery;
+    private final List<Photo> values;
+    private PhotosDao dao;
+
+    PhotoAdapter(Callback<Response> parent, List<Photo> items) {
+        photoGallery = parent;
+        values = items;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull PhotoAdapter.ViewHolder holder, int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
 
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        String s;
+        s = "https://farm" + Integer.toString(values.get(position).getFarm()) + ".staticflickr.com/" +
+                values.get(position).getServer() + "/" + values.get(position).getId() +
+                "_" + values.get(position).getSecret() + "_q.jpg";
+        Picasso.get().load(s).into(holder.idView);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return values.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        final ImageView idView;
 
         ViewHolder(View view) {
             super(view);
+            idView = view.findViewById(R.id.iv);
         }
     }
 }
--- app/src/main/java/com/example/photogallery/PhotoGallery.java
+++ app/src/main/java/com/example/photogallery/PhotoGallery.java
@@ -6,6 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.photogallery.api.FlickrAPI;
+import com.example.photogallery.api.ServiceAPI;
+import com.example.photogallery.model.Response;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+
 public class PhotoGallery extends AppCompatActivity {
 
     RecyclerView rv;
@@ -16,7 +24,20 @@ public class PhotoGallery extends AppCompatActivity {
         setContentView(R.layout.gallery_activity);
 
         rv = findViewById(R.id.rv);
-        rv.setAdapter(new PhotoAdapter());
         rv.setLayoutManager(new GridLayoutManager(this, 3));
+
+        Retrofit r = ServiceAPI.getRetrofit();
+        r.create(FlickrAPI.class).getRecent().enqueue(new Callback<Response>()
+        {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                PhotoAdapter pa = new PhotoAdapter(this, response.body().getPhotos().getPhoto());
+                rv.setAdapter(pa);
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+            }
+        });
     }
 }
