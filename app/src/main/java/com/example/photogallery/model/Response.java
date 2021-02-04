8b899614dabbfed28413f03ada47fad14bbc3cb3
IpMan522 <mihailanatolevic844@gmail.com>
2021-02-04 12:55:50 +0700

    Добавил модели для парсинга json

---
+++ app/src/main/java/com/example/photogallery/model/Response.java
@@ -0,0 +1,28 @@
+package com.example.photogallery.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Response {
+    private Photos photos;
+
+    private String stat;
+
+    public Photos getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(Photos photos) {
+        this.photos = photos;
+    }
+
+    public String getStat() {
+        return stat;
+    }
+
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+}
